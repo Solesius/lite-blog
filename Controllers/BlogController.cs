5f708aaf38d7bee16b9a193a0edeb5e96dc2318d
= <=>
2024-04-16 10:05:33 -0400

    setup files

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace net.Controllers;
+
+[ApiController]
+[Route("api/blog")]
+public class BlogController : ControllerBase
+{
+    private readonly ILogger<BlogController> _logger;
+
+    public BlogController(ILogger<BlogController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("/list")]
+    public IActionResult ListBlog()
+    {
+        return Ok("");
+    }
+}
